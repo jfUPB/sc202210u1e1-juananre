77e31501eef9942892ba6c6304170cddfd804d16
juananre <78058130+juananre@users.noreply.github.com>
2022-02-09 08:41:24 -0500

    Update program.asm

--- problem/program.asm
+++ problem/program.asm
@@ -1,6 +1,6 @@
-// COLOCO AQUÍ TU NOMBRE COMPLETO
-// COLOCA AQUÍ TU ID
-// COLOCA AQUÍ TU CORREO ELECTRÓNICO
+// Juan Anderes Ferrer Dueñas
+// 000421702
+// juan.ferrer@upb.edu.co
 (START)
     @START
     0;JMP
